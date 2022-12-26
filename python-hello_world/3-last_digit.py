a73fb0acfe2137e7a52ff34f22756ef3cf4a4d79
root <vignanp@koderoom.com>
2022-12-26 09:46:52 +0000

    3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -5,9 +5,12 @@ l = number % 10
 if number < 0:
     l = l - 10
 if l == 0:
-    print("Last digit of " + str(number) + " is " + str(l) + " and is 0")
+    print("Last digit of " + str(number) +
+          " is " + str(l) + " and is 0")
 else:
     if l > 5:
-        print("Last digit of " + str(number) + " is " + str(l) + " and is greater than 5")
+        print("Last digit of " + str(number) +
+              " is " + str(l) + " and is greater than 5")
     elif l < 5:
-        print("Last digit of " + str(number) + " is " + str(l) + " and is less than 6 and not 0")
+        print("Last digit of " + str(number) +
+              " is " + str(l) + " and is less than 6 and not 0")
